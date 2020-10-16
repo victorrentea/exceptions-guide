aa62125b107c51734b4d80d074c1acae004c7b95
Victor Rentea <victorrentea@gmail.com>
2020-10-17 00:03:43 +0300

    Initial commit

---
+++ quarkus/src/main/java/org/acme/GlobalExceptionMapper.java
@@ -0,0 +1,15 @@
+package org.acme;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
+
+  @Override
+  public Response toResponse(Exception ex) {
+    return Response.status(400)
+        .entity("Mapper A: " + ex.getMessage())
+        .build();
+  }
+}
---
+++ quarkus/src/main/java/org/acme/HelloResource.java
@@ -0,0 +1,25 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+
+        if (true) {
+            throw new IllegalArgumentException();
+        }
+        return "hello3";
+    }
+}
+
+
