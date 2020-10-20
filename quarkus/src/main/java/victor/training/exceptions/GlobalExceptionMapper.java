d86af097a08dcd79e974b9e9f91316b40637e142
Victor Rentea <victorrentea@gmail.com>
2020-10-20 11:57:38 +0300

    START: javax GlobalExceptionMapper

---
+++ quarkus/src/main/java/victor/training/exceptions/GlobalExceptionMapper.java
@@ -0,0 +1,13 @@
+package victor.training.exceptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
+   @Override
+   public Response toResponse(Exception exception) {
+      return Response.serverError().entity(exception.getMessage()).build();
+   }
+}
