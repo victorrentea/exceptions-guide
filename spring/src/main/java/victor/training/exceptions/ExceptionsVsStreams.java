66c8f2a751a5322ea33d2ce8ac07399784f409ea
Victor Rentea <victorrentea@gmail.com>
2020-11-17 00:32:56 +0200

    dateList

--- spring/src/main/java/victor/training/exceptions/ExceptionsVsStreams.java
+++ spring/src/main/java/victor/training/exceptions/ExceptionsVsStreams.java
@@ -14,7 +14,7 @@ import static java.util.stream.Collectors.toList;
 public class ExceptionsVsStreams {
 
    public static void main(String[] args) {
-      List<String> dateStr = asList("2020-10-11", "2020-nov-12", "2020-12-01");
+      List<String> dateList = asList("2020-10-11", "2020-nov-12", "2020-12-01");
       SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
       // ====== jool ======
