6ff0397a4f576afc3213d78d4ee1d98d22fb8e27
Victor Rentea <victorrentea@gmail.com>
2020-11-17 00:31:58 +0200

    prepare

--- spring/src/main/java/victor/training/exceptions/ExceptionsVsStreams.java
+++ spring/src/main/java/victor/training/exceptions/ExceptionsVsStreams.java
@@ -1,11 +1,12 @@
 package victor.training.exceptions;
 
 import io.vavr.control.Try;
+import org.jooq.lambda.Unchecked;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
@@ -13,24 +14,13 @@ import static java.util.stream.Collectors.toList;
 public class ExceptionsVsStreams {
 
    public static void main(String[] args) {
-      List<String> list = asList("2020-10-10", "2020-10-c", "2020-10-b", "2020-10-c", "2020-10-15");
-      System.out.println(parseDates(list));
-   }
+      List<String> dateStr = asList("2020-10-11", "2020-nov-12", "2020-12-01");
+      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
-   private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); // WARNING: NOT Thread-safe!
-   static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+      // ====== jool ======
 
-   public static List<LocalDate> parseDates(List<String> dateStrings) {
-      // Change Request: return parsable dates if >=50%
 
-      List<Try<LocalDate>> tries = dateStrings.stream().map(s -> Try.of(() -> LocalDate.parse(s, formatter))).collect(toList());// TODO
-
-      if (tries.stream().filter(Try::isSuccess).count() * 2 >= dateStrings.size()) {
-         return tries.stream().filter(Try::isSuccess).map(Try::get).collect(toList());
-      } else {
-         List<String> errors = tries.stream().filter(Try::isFailure).map(Try::getCause).map(Throwable::getMessage).collect(toList());
-         throw new IllegalArgumentException(errors.toString());
-      }
+      // ====== vavr: Accept if >= 50% parseable =====
    }
 
 }
