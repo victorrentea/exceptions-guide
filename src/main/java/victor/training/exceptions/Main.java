480f8d751e1a49fe2a4082dac1307e19456ff141
Victor Rentea <victorrentea@gmail.com>
2020-11-17 23:58:08 +0200

    fix

--- src/main/java/victor/training/exceptions/Main.java
+++ src/main/java/victor/training/exceptions/Main.java
@@ -7,7 +7,7 @@ public class Main {
    public static void main(String[] args) {
       Order order = new Order().setPrice(1000);
 
-      new Biz().applyDiscount(new Order().setPrice(1000), new Customer());
+      new Biz().applyDiscount(order, new Customer());
 
       System.out.println("Discounted Price " + order.getPrice());
    }
