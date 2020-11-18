2469ac9ddf0034f96ec5b65f6e22cbd2e6b96a65
Victor Rentea <victorrentea@gmail.com>
2020-11-18 16:58:03 +0200

    TODO

--- src/main/java/victor/training/exceptions/InProduction.java
+++ src/main/java/victor/training/exceptions/InProduction.java
@@ -3,7 +3,7 @@ package victor.training.exceptions;
 import victor.training.exceptions.model.Customer;
 import victor.training.exceptions.model.Order;
 
-public class Main {
+public class InProduction {
    public static void main(String[] args) {
       Order order = new Order().setPrice(1000);
 
