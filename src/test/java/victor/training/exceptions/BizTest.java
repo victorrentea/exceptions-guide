1b7ce886204295c99fd24f7296e328a8209f64fd
Victor Rentea <victorrentea@gmail.com>
2020-11-19 14:16:51 +0200

    cleanup

--- src/test/java/victor/training/exceptions/BizTest.java
+++ src/test/java/victor/training/exceptions/BizTest.java
@@ -8,17 +8,22 @@ import victor.training.exceptions.model.Order;
 
 import java.util.Date;
 
+import static org.junit.Assert.assertEquals;
+
 public class BizTest {
    @Test
    public void test() {
       System.setProperty("last.promo.date", "2021-02-30");
       Biz biz = new Biz();
 
-      Customer customer = new Customer().setMemberCard(new MemberCard().setFidelityDiscount(2));
+      Customer customer = new Customer()
+          .setMemberCard(new MemberCard()
+              .setFidelityDiscount(2));
       Order order = new Order().setOfferDate(new Date())
           .setPrice(1000);
 
       biz.applyDiscount(order, customer);
-      Assert.assertEquals(960, order.getPrice());
+      System.out.println("Final Price in tests: " + order.getPrice());
+      assertEquals(960, order.getPrice());
    }
 }
