e0a6107fedeba9fe3ed6cf3278b69fbf435540a2
Victor Rentea <victorrentea@gmail.com>
2020-11-19 19:16:02 +0200

    Try

--- src/main/java/victor/training/exceptions/Streams.java
+++ src/main/java/victor/training/exceptions/Streams.java
@@ -14,11 +14,37 @@ import static java.util.stream.Collectors.toList;
 public class Streams {
 
    public static void main(String[] args) {
-      List<String> dateList = asList("2020-10-11", "2020-nov-12", "2020-12-01");
+      List<String> dateList = asList("2020-10-11", "2020-nov-12", "2020-xxx12-01");
       SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
       // TODO parse and print all dates
 
+//      List<Date> dates = dateList.stream().map(Unchecked.function(format::parse)).collect(toList());
+
+
+      List<Try<Date>> tries = dateList.stream().map(s -> Try.of(() -> format.parse(s))).collect(toList());
+
+      if (tries.stream().mapToInt(t->t.isSuccess()?1:0).average().getAsDouble() > .5) {
+         List<Date> dates1 = tries.stream().filter(Try::isSuccess).map(Try::get).collect(toList());
+         System.out.println(dates1);
+      }
+
+
    }
+
+//   private static <T,R> Function<T, R> wrapEx(ThrowingFunction<T, R> f) {
+//      return t -> {
+//         try {
+//            return f.apply(t);
+//         } catch (Throwable throwable) {
+//            throw new RuntimeException(throwable);
+//         }
+//      };
+//   }
+//
+//   interface ThrowingFunction<T, R> {
+//      R apply(T t) throws Throwable;
+//   }
+
 
 }
