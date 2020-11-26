eddc9a7a217ba91a5cf13a43c2051edea1393d64
Victor Rentea <victorrentea@gmail.com>
2020-11-26 21:58:18 +0200

    catch (ParseException) {// TODO } - never. At least log.error

--- src/main/java/victor/training/exceptions/Biz.java
+++ src/main/java/victor/training/exceptions/Biz.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import victor.training.exceptions.model.Customer;
 import victor.training.exceptions.model.Order;
 
+import java.text.ParseException;
 import java.util.Date;
 
 /**
@@ -14,12 +15,15 @@ import java.util.Date;
 public class Biz {
 
    public void applyDiscount(Order order, Customer customer) {
-      // TODO offer date < Config.getLastPromoDate()
-      if (order.getOfferDate().before(DateUtils.addDays(new Date(), 1))) {
-         System.out.println("APPLYING DISCOUNT");
-         order.setPrice(order.getPrice() * (100 - 2 * customer.getMemberCard().getFidelityDiscount()) / 100);
-      } else {
-         System.out.println("NO DISCOUNT");
+      try {
+         if (order.getOfferDate().before(Config.getLastPromoDate())) {
+            System.out.println("APPLYING DISCOUNT");
+            order.setPrice(order.getPrice() * (100 - 2 * customer.getMemberCard().getFidelityDiscount()) / 100);
+         } else {
+            System.out.println("NO DISCOUNT");
+         }
+      } catch (ParseException e) {
+         // TODO
       }
    }
 }
