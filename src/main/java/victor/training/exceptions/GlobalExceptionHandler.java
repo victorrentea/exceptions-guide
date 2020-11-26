e557fd936af400ae6147a726478c8a3b584d7042
Victor Rentea <victorrentea@gmail.com>
2020-11-26 23:21:54 +0200

    move to Spring Boot webapp. Introduce Global Exception Handler.

---
+++ src/main/java/victor/training/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package victor.training.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+   private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+   private final MessageSource messageSource;
+
+   public GlobalExceptionHandler(MessageSource messageSource) {
+      this.messageSource = messageSource;
+   }
+
+   @ExceptionHandler(MyException.class)
+   @ResponseStatus
+   public String handleMyException(MyException exception, HttpServletRequest request) {
+      String userMessage = messageSource.getMessage(exception.getCode().name(), null, request.getLocale());
+      log.error(userMessage, exception);
+      return userMessage;
+   }
+}
