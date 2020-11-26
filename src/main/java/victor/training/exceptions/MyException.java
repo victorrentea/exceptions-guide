7aa14bd5215077a415db4cffb05c3bebb0a7405b
Victor Rentea <victorrentea@gmail.com>
2020-11-26 23:41:28 +0200

    Additional overloaded constructor for convenience

--- src/main/java/victor/training/exceptions/MyException.java
+++ src/main/java/victor/training/exceptions/MyException.java
@@ -9,16 +9,35 @@ public class MyException extends RuntimeException {
    private final ErrorCode code;
    private final Object[] params;
 
-   public MyException(ErrorCode code, Throwable cause, Object... params) {
-      super(cause);
+   // canonical constructor
+   public MyException(String message, ErrorCode code, Throwable cause, Object... params) {
+      super(message, cause);
       this.code = code;
       this.params = params;
+   }
+   // overloaded constructors:
+   public MyException(ErrorCode code, Throwable cause, Object... params) {
+      this(null, code, cause, params);
+   }
+   public MyException(ErrorCode code, Object... params) {
+      this(null, code, null, params);
+   }
+   public MyException(String message, ErrorCode code, Object... params) {
+      this(message, code, null, params);
+   }
+   public MyException(String message, Throwable cause) {
+      this(message, ErrorCode.GENERAL, cause);
+   }
+   public MyException(Throwable cause) {
+      this(ErrorCode.GENERAL);
+   }
+   public MyException(String message) {
+      this(message, ErrorCode.GENERAL);
    }
 
    public ErrorCode getCode() {
       return code;
    }
-
    public Object[] getParams() {
       return params;
    }
