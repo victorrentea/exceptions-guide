e7893a8e5721e2871546efaf15a4226927589383
Victor Rentea <victorrentea@gmail.com>
2020-11-27 03:08:38 +0200

    @SneakyThrows on my function -> no more checked exceptions :)

--- src/main/java/victor/training/exceptions/Streams.java
+++ src/main/java/victor/training/exceptions/Streams.java
@@ -1,6 +1,7 @@
 package victor.training.exceptions;
 
 import io.vavr.control.Try;
+import lombok.SneakyThrows;
 import org.jooq.lambda.Unchecked;
 
 import java.text.ParseException;
@@ -18,16 +19,13 @@ public class Streams {
       List<String> dateList = asList("2020-10-11", "2020-nov-12", "2020-12-01");
       SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
-      List<Date> dates = dateList.stream().map(s -> {
-         try {
-            return format.parse(s);
-         } catch (ParseException e) {
-            throw new RuntimeException(e);
-         }
-      }).collect(toList());
-
+      List<Date> dates = dateList.stream().map(s -> uglyParse(format, s)).collect(toList());
       System.out.println(dates);
+   }
 
+   @SneakyThrows
+   private static Date uglyParse(SimpleDateFormat format, String s) {
+      return format.parse(s);
    }
 
 }
