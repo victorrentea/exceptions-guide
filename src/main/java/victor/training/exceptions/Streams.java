cf6ed3cde55547142d199aa8c993c10900ad383a
Victor Rentea <victorrentea@gmail.com>
2020-11-27 03:16:35 +0200

    Unchecked (jool) library

--- src/main/java/victor/training/exceptions/Streams.java
+++ src/main/java/victor/training/exceptions/Streams.java
@@ -1,5 +1,7 @@
 package victor.training.exceptions;
 
+import org.jooq.lambda.Unchecked;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -14,24 +16,9 @@ public class Streams {
       List<String> dateList = asList("2020-10-11", "2020-nov-12", "2020-12-01");
       SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
-      ThrowingFunction<String, Date> p = s -> format.parse(s);
-      Function<String, Date> f = wrapAsRuntime(p); // TODO
-      List<Date> dates = dateList.stream().map(f).collect(toList());
+      // TODO
+      List<Date> dates = dateList.stream().map(Unchecked.function(format::parse)).collect(toList());
       System.out.println(dates);
-   }
-
-   private static <T,R> Function<T, R> wrapAsRuntime(ThrowingFunction<T, R> p) {
-      return t -> {
-         try {
-            return p.apply(t);
-         } catch (Exception e) {
-            throw new RuntimeException(e);
-         }
-      };
-   }
-
-   interface ThrowingFunction<T,R> {
-      R apply(T t) throws Exception;
    }
 
 }
