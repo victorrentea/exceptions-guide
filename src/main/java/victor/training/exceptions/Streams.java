db0c581edcf25ce148d966727526f00442fc4056
Victor Rentea <victorrentea@gmail.com>
2020-11-27 03:58:26 +0200

    vavr.Try<>

--- src/main/java/victor/training/exceptions/Streams.java
+++ src/main/java/victor/training/exceptions/Streams.java
@@ -1,5 +1,6 @@
 package victor.training.exceptions;
 
+import io.vavr.control.Try;
 import org.jooq.lambda.Unchecked;
 
 import java.text.SimpleDateFormat;
@@ -17,8 +18,19 @@ public class Streams {
       SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
       // TODO
-      List<Date> dates = dateList.stream().map(Unchecked.function(format::parse)).collect(toList());
-      System.out.println(dates);
+      List<Try<Date>> tries = dateList.stream().map(s -> tryParse(format, s)).collect(toList());
+
+      double successRatio = tries.stream().mapToInt(t -> t.isSuccess() ? 1 : 0).average().orElse(0);
+
+      if (successRatio > .5) {
+         List<Date> dates = tries.stream().filter(Try::isSuccess).map(Try::get).collect(toList());
+         System.out.println(dates);
+      }
+
+   }
+
+   private static Try<Date> tryParse(SimpleDateFormat format, String s) {
+      return Try.of(() -> format.parse(s));
    }
 
 }
