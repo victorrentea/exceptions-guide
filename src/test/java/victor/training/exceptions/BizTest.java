2979834da8ff54d57aa13e860854251246b2c208
Victor Rentea <victorrentea@gmail.com>
2021-01-19 03:08:59 +0200

    improve startup

--- src/test/java/victor/training/exceptions/BizTest.java
+++ src/test/java/victor/training/exceptions/BizTest.java
@@ -1,29 +1,29 @@
-package victor.training.exceptions;
-
-import org.junit.Assert;
-import org.junit.Test;
-import victor.training.exceptions.model.Customer;
-import victor.training.exceptions.model.MemberCard;
-import victor.training.exceptions.model.Order;
-
-import java.util.Date;
-
-import static org.junit.Assert.assertEquals;
-
-public class BizTest {
-   @Test
-   public void test() {
-      System.setProperty("last.promo.date", "2021-02-30");
-      Biz biz = new Biz();
-
-      Customer customer = new Customer()
-          .setMemberCard(new MemberCard()
-              .setFidelityDiscount(2));
-      Order order = new Order().setOfferDate(new Date())
-          .setPrice(1000);
-
-      biz.applyDiscount(order, customer);
-      System.out.println("Final Price in tests: " + order.getPrice());
-      assertEquals(960, order.getPrice());
-   }
-}
+//package victor.training.exceptions;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//import victor.training.exceptions.model.Customer;
+//import victor.training.exceptions.model.MemberCard;
+//import victor.training.exceptions.model.Order;
+//
+//import java.util.Date;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//public class BizTest {
+//   @Test
+//   public void test() {
+//      System.setProperty("last.promo.date", "2021-02-30");
+//      Biz biz = new Biz();
+//
+//      Customer customer = new Customer()
+//          .setMemberCard(new MemberCard()
+//              .setFidelityDiscount(2));
+//      Order order = new Order().setOfferDate(new Date())
+//          .setPrice(1000);
+//
+//      biz.applyDiscount(order, customer);
+//      System.out.println("Final Price in tests: " + order.getPrice());
+//      assertEquals(960, order.getPrice());
+//   }
+//}
